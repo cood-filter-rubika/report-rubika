b401c561d554231ed89349423ee68662f2cdc769
maxtor <96935075+cood-filter-rubika@users.noreply.github.com>
2022-01-17 07:42:41 +0330

    Add files via upload

---
+++ reporter-maxtor.py
@@ -0,0 +1,423 @@
+import os
+import time
+import requests
+from threading import Thread
+from colorama import Fore
+from time import sleep
+print(Fore.YELLOW + "")
+print("""# report #
+                           .7KBQBBBBBBBBBIr
+                        i5RQQP1ri..   ..i7IbQQM2i
+                    rQBBDY.                   .JgBBMr
+                 rBQB7                             vBBBi
+               PBBr                                   vBQI
+             ZBD.                                       .QBX
+           IBg                                             BBv
+          BB.                                               .BB
+        vBY      qq.       ...:r::i.::i::i:..        :dI      XBi
+       DB     :BBR      ..: .:.  :  : ..  ::....       BBB.    .B2
+      BB   g:1BBi     .r:   .   ..  :  :.   .   :i.     7BQ7rd   Bb
+     gB   BE.BIi:   .:. ..:i.   i   i   :   .i:.. .:.   i:PB BB   BI
+    YB   BB :iIB   :.    ..  ..:: dd7BQ:.:..  ..    .:  .BJi. Qd   Bi
+    B  B BQ QBB   i     .:     :  BR XBB .     :.     i   BBD BB.B :B
+   BM PB BQBU:   :      i      i    :BM  i      i      i   iXBBB Bv BB
+  .B  BB Qu P7  i....  :.     ..    X    :      ..  ....i  u1 qg BB  B
+  BB  BB  rBB  i    ...i....  :.   .5    :. .....i...    :  BBi .BB  Bj
+  B   BB DBZ  .:      ..   .i...# maxtor #...: .    :       :   BBI.BZ  iB
+ jQ Y: BQB..  :       :.      :           .      ..      .:   :BMB Y: B
+ BB MB RB  B  i       :       .   rEXEi   .       :       :  B  BS B1 B
+ BZ vB: : BP  i.......:        1B .QBd  Bv       .:.......:  BB r vB: B
+ BM  BB  BB   i . . . :  .:rUZBBB  rB:  BBBE1i:.  : ... . i   BB .BB  B
+ BB  KBgiBY   :        rBBBBBBQB.   Q   iBBBBBBBBi        :   KB:BBL  B
+ vB L YBBB Qi :        BBBBQBBBB:  5B7  7BBBBBBBBZ       .: LK BQQi r B
+  B rB  BR Bd  :       BQBQBQBBBQ  MQS  QBQBBBBBBB       :  BB BB  B.:B
+  QQ BB. : QB  :    . :BBBBBBBBBB7 bBs 5BBBBBBBBBB. .   ..  BB i :BB BY
+   B  BBB  BQ i :...  :BBBBBQBBBBB:KBJiBBBBBBBBBBB   ..:. r BB  BBQ  B
+   BB  uBBiBB Bj .    PBBQBBBBBQBBBBBQBBBBBBBQBQBBJ    . dB BRrBB7  BQ
+    B. 7 vBBQ rB: .   QBBBBBBBBBBBBBQBBBBBBBBBBBBBZ   . 7B..BBBr 7 7B
+    7B iB.  K5 BB     BQBBBBBQBQBBBBBBBQBBBBBBBBBBB     BB D1  iB. Q:
+     bQ .BBP:: YBd 5  BBQBBBBBBBQBQBBBQBBBBBQBBBBBB  X BBi ::gBB  BJ
+      gB  rBBBBrZB.uB7BBBBBBBBBQBBBBBQBBBQBBBBBBBQBrBi:QqrBBBQ:  BI
+       KB.  ivqBMBB 1BBBBBBBBBBBBBBBBBBBBBBBBBBBBBQB7 BBMBSvi  :BY
+        rQ5 .u7:. :r .KQBBBBBBBBBQBQBBBBBBBBBBBQBQS  7: .:vJ  DB:
+          QB  iBBBBBBBBBQKrQQBBBBBBBBBBBBBBBQZrPQBBBBBBBBQ: .BB
+           7Bg   :i:..    iQBBBBBBBQBQBBBBBBBRi    ..:r.   BBi
+             XBR  .2ZQBBBBBBQBBBBBBBBBBBBBBBQBBBBBBQE1. .BBu
+               uBQi  .r7i  BBQBBBBBQBBBBBBBBBQ .rri.  rBBv
+                 :gBgi     BQBBBBBBBQBBBBBBBBB     rMBZ.
+                    idBBgrJBBBBBBBBBBBBBBBBBBB77QBBP:
+                        .rv1XZMQQBBBBBBBQQgZSu7i
+                                  bay""")
+print(Fore.RED)
+print("id Rubika target ro ward kon in payen bedon @ --")
+id = input()
+os.system("clear")
+print(Fore.GREEN)
+print(F"ok attack sroh shod be @{id} filter Rubika.ir")
+#!/usr/bin/python
+
+print("null×")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(4)
+
+print("null×")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("null×")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("Ok[]")
+time.sleep(3)
+
+print("block ip accunt rubika")
